228ee951136d486ed444d3f59ed0f63ff9aadf7b
gardusig <gustavo.gardusi@gmail.com>
2023-01-08 21:44:01 -0300

    Added worker util

---
+++ internal/worker/worker_util.go
@@ -0,0 +1,47 @@
+package worker
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/conductor-sdk/conductor-go/sdk/model"
+	"github.com/conductor-sdk/conductor-go/sdk/worker"
+
+	"github.com/conductor-sdk/go-sdk-examples/internal/api"
+)
+
+var taskRunner = worker.NewTaskRunnerWithApiClient(
+	api.GetApiClientWithAuthentication(),
+)
+
+const (
+	batchSize    = 5
+	pollInterval = 100 * time.Millisecond
+)
+
+func StartWorkers() {
+	startWorker("get_user_info", GetUserInfo)
+	startWorker("send_email", SendEmail)
+	startWorker("send_sms", SendSms)
+}
+
+func GetValueFromTaskInputData(t *model.Task, key string) (value *string, err error) {
+	rawValue, ok := t.InputData[key]
+	if !ok {
+		return nil, fmt.Errorf("key (%s) is not set", key)
+	}
+	*value, ok = rawValue.(string)
+	if !ok {
+		return nil, fmt.Errorf("key (%s) value must be of string type", key)
+	}
+	return value, nil
+}
+
+func startWorker(taskName string, executeFunction model.ExecuteTaskFunction) {
+	taskRunner.StartWorker(
+		taskName,
+		executeFunction,
+		batchSize,
+		pollInterval,
+	)
+}
