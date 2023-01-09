9976758384746c24cd06dae3ca174d517c7541a0
gardusig <gustavo.gardusi@gmail.com>
2023-01-08 21:44:31 -0300

    Updated main

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/conductor-sdk/go-sdk-examples/internal/worker"
+)
+
+func setupLogSettings() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+	log.Info("Finished setting up log settings")
+}
 
 func main() {
-	fmt.Println("hello world!")
+	setupLogSettings()
+	worker.StartWorkers()
+	time.Sleep(5 * time.Second)
 }
